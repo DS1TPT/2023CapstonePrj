74b33fd232be7cbaee9b90c864f5f88bfb2c516e
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-01-13 17:52:40 +0900

    Create scheduler.h

---
+++ Inc/scheduler.h
@@ -0,0 +1,49 @@
+/**
+  *********************************************************************************************
+  * NAME OF THE FILE : scheduler.c
+  * BRIEF INFORMATION: auto-play schedule manager
+  *
+  * !ATTENTION!
+  *
+  * Copyright (c) 2023 Lee Geon-goo.
+  * All rights reserved.
+  *
+  * This file is part of catCareBot.
+  * catCareBot is free software: you can redistribute it and/or modify it under the
+  * terms of the GNU General Public License as published by the Free Software Foundation,
+  * either version 3 of the License, or (at your option) any later version.
+  *
+  * catCareBot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  * See the GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License along with catCareBot.
+  * If not, see <https://www.gnu.org/licenses/>.
+  *
+  *********************************************************************************************
+  */
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+/* includes */
+#include "main.h"
+#include "rpicomm.h"
+
+/* exported struct */
+struct patternQueue {
+	unsigned count;
+	uint8_t queue[70];
+};
+
+/* exported vars */
+
+/* exported func prototypes */
+void scheduler_init();
+void scheduler_setSpd(uint8_t spd); // set speed of robot
+int scheduler_setTime(int32_t sec); // set time and start timer. returns OK on success, ERR on failure
+int scheduler_enqueuePattern(uint8_t code); // returns ERR if queue is full, returns OK otherwise
+int scheduler_dequeuePattern(); // returns ERR if queue is empty, returns OK otherwise
+int scheduler_TimCallbackHandler(); // returns 0 if time is not elapsed or time is not set. returns 1 if elapsed
+
+#endif
