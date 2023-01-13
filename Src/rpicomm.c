e75d3972c9de063925f93619e600ab402d6501b2
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-01-13 17:52:11 +0900

    Update rpicomm.c

--- Src/rpicomm.c
+++ Src/rpicomm.c
@@ -34,7 +34,7 @@
  * 		if schedule is set, sleep mode will be active.
  * 		this behavior is for keeping pattern stack active.
  * 		CON\0\0\0\0: this will set RUN mode.
- * SCHEDULE_TIME: DWORD 00 00 00. Use uint32.
+ * SCHEDULE_SPEED: DWORD 00 00 00. Use SIGNED int32.
  * SCHEDULE_PATTERN: pattern code, 4b per pattern.
  * 					therefore, a packet can contain up to 14 patterns.
  * 					NOTE: pattern stack can contain up to 70 patterns.
@@ -45,28 +45,36 @@
  * 				  format: BYTE 00 00 00 00 00 00. uint8
  * 				  SYS: system commands. not defined this time
  * MANUAL_CTRL: format: BYTE 00 00 00 00 00 00. uint8
- * 0x80: undefined
+ * SCHEDULE_START: FF 00 FF 00 FF 00 FF
+ * SCHEDULE_END: 00 FF 00 FF 00 FF 00
+ * RESP: BYTE 00 00 00 00 00 00
+ *
+ * for more info, go to <https://github.com/DS1TPT/catCareBot> and check document pktDefs.pdf
  */
 
 #include "main.h"
 #include "rpicomm.h"
 
 struct SerialDta uartdta;
-uint8_t pindta = 0;
-uint8_t rxBuf[9] = 0;
+uint8_t rxBuf[9] = { 0, };
+uint8_t txBuf[8] = { 0, };
 
 int rpi_getSerialDta(struct SerialDta dest) {
 	if (uartdta.available) { // has new received data
-		uartdta.type = rxBuf[0]; // copy from buf to internal var
-		for (int i = 0; i < 8; i++)
-			uartdta.container[i] = rxBuf[i + 1];
-		dest = uartdta; // copy from internal buffer to dest
-		uartdta.available = 0;
-		for (int i = 0; i < 9; i++) // clr buf
-			rxBuf[i] = 0;
+		dest = uartdta; // copy from internal var to dest var
+		uartdta.available = 0; // mark unavailable
 		return 1;
 	}
 	else return 0;
+}
+
+uint8_t rpi_getPinDta() {
+	uint8_t dta = 0;
+	if (HAL_GPIO_ReadPin(RPI_SIG, FOUND_CAT) == GPIO_PIN_SET)
+		dta |= 1;
+	else dta &= ~0x01;
+	// add more code as needed
+	return dta;
 }
 
 void rpi_init() {
@@ -77,9 +85,28 @@ void rpi_init() {
 	HAL_UART_Receive_IT(&huart1, &rxBuf, 9);
 }
 
+void rpi_RxCpltCallbackHandler() {
+	uartdta.available = 1; // mark available
+	uartdta.type = rxBuf[0]; // copy data from buffer to internal var
+	for (int i = 0; i < 8; i++)
+		uartdta.container[i] = rxBuf[i + 1];
+	for (int i = 0; i < 9; i++) // clr buf
+		rxBuf[i] = 0;
+	HAL_UART_Receive_IT(&huart1, &rxBuf, 9); // restart rx
+}
+
+int rpi_tcpipAvailable() { // returns zero if not available
+
+}
+int rpi_tcpipRespond() { // send RESP pkt to client app. returns 0 on success
+
+}
+
+/* ADD THIS TO MAIN.C
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 	if (huart->Instance == USART1) {
-		uartdta.available = 1;
-		HAL_UART_Receive_IT(&huart1, &rxBuf, 9);
+		rpi_RxCpltCallbackHandler();
+		flagRxCplt = TRUE;
 	}
 }
+*/
