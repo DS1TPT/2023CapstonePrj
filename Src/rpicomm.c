41045e269e9540405ea1aa9a0d00d994c02e8d8e
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-02-04 16:49:22 +0900

    Update rpicomm.c

--- Src/rpicomm.c
+++ Src/rpicomm.c
@@ -4,22 +4,10 @@
   * BRIEF INFORMATION: for communications between stm32f411re nucleo board
   * 			  	   and raspberry pi via GPIO pins and UART.
   *
-  * !ATTENTION!
-  *
   * Copyright (c) 2023 Lee Geon-goo.
   * All rights reserved.
   *
   * This file is part of catCareBot.
-  * catCareBot is free software: you can redistribute it and/or modify it under the
-  * terms of the GNU General Public License as published by the Free Software Foundation,
-  * either version 3 of the License, or (at your option) any later version.
-  *
-  * catCareBot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  * See the GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License along with catCareBot.
-  * If not, see <https://www.gnu.org/licenses/>.
   *
   *********************************************************************************************
   */
