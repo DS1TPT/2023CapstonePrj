bf387129f5679f7cd09b9b0f4ef532df546795e4
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-02-04 16:47:24 +0900

    Update rpicomm.h

--- Inc/rpicomm.h
+++ Inc/rpicomm.h
@@ -4,22 +4,10 @@
   * BRIEF INFORMATION: for communications between stm32f411re nucleo board
   * 			  	   and raspberry pi via GPIO pins and UART.
   *
-  * !ATTENTION!
-  *
   * Copyright (c) 2023 Lee Geon-goo.
   * All rights reserved.
   *
   * This file is part of catCareBot.
-  * catCareBot is free software: you can redistribute it and/or modify it under the
-  * terms of the GNU General Public License as published by the Free Software Foundation,
-  * either version 3 of the License, or (at your option) any later version.
-  *
-  * catCareBot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-  * See the GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License along with catCareBot.
-  * If not, see <https://www.gnu.org/licenses/>.
   *
   *********************************************************************************************
   */
@@ -35,9 +23,12 @@
 #define TYPE_SCHEDULE_TIME 2
 #define TYPE_SCHEDULE_PATTERN 4
 #define TYPE_SCHEDULE_SNACK 8
-#define TYPE_SCHEDULE_SPEED 16
-#define TYPE_SYS 32
-#define TYPE_MANUAL_CTRL 64
+#define TYPE_SCHEDULE_SPEED 0x10
+#define TYPE_SYS 0x20
+#define TYPE_MANUAL_CTRL 0x40
+#define TYPE_SCHEDULE_START 0xAA
+#define TYPE_SCHEDULE_END 0x55
+#define TYPE_RESP 0xFF
 
 /* exported struct */
 struct SerialDta {
