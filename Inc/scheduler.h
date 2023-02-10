0420e329fe6a96a19370a835291dc06d0d71e078
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-02-10 19:10:12 +0900

    Update scheduler.h

--- Inc/scheduler.h
+++ Inc/scheduler.h
@@ -26,6 +26,7 @@
 void scheduler_init();
 void scheduler_setSpd(uint8_t spd); // set speed of robot
 void scheduler_setTime(int32_t sec); // set time and start timer. returns OK on success, ERR on failure
+void scheduler_setDuration(int32_t sec); // set play time
 int scheduler_setSnack(uint8_t num); // set how many times the user want to give treat
 int scheduler_enqueuePattern(uint8_t code); // returns ERR if queue is full, returns OK otherwise
 int scheduler_dequeuePattern(); // returns ERR if queue is empty, returns OK otherwise
@@ -33,5 +34,6 @@ int scheduler_TimCallbackHandler(); // returns 0 if time is not elapsed or time 
 int scheduler_isSet(); // check if schedule is set and complete, returns 0 if schedule is not set
 uint8_t scheduler_getSpd(); // return speed to core
 uint8_t scheduler_getSnack(); // return how many times the user want to give treat to core
+int32_t scheduler_getInterval(); // return interval time between patterns
 
 #endif
