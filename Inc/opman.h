7b32dfaa6280b903dfc4c3b9e37060faec8d94ad
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-03-16 20:41:38 +0900

    Update opman.h

--- Inc/opman.h
+++ Inc/opman.h
@@ -21,8 +21,6 @@
 #define OP_SNACK_RET_MOTOR 0x01
 #define OP_RPI_PIN_IO_SEND_RESET 0x02
 
-#define OPMAN_TIM TIM11
-
 void opman_setHandle(TIM_HandleTypeDef* ph);
 void opman_init();
 void opman_addPendingOp(uint8_t opcode, uint16_t milliseconds); // opcode 0 will be ignored
