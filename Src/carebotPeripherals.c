e7ece0cd032c12057d058e8f77754a1cc2f66293
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-03-18 22:32:30 +0900

    Update carebotPeripherals.c

--- Src/carebotPeripherals.c
+++ Src/carebotPeripherals.c
@@ -1,7 +1,7 @@
 /**
   *********************************************************************************************
   * NAME OF THE FILE : carebotPeripherals.c
-  * BRIEF INFORMATION: peripheral device driver
+  * BRIEF INFORMATION: drive peripheral devices
   *
   * Copyright (c) 2023 Lee Geon-goo.
   * All rights reserved.
