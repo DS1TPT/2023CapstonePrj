0541ad36dc4c1a055aad5e6f9596751907b45681
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-05-15 00:26:06 +0900

    Update rpicomm.c

--- Src/rpicomm.c
+++ Src/rpicomm.c
@@ -36,13 +36,10 @@ int rpi_getSerialDta(struct SerialDta* pDest) {
 	else return 0;
 }
 
-uint8_t rpi_getPinDta() {
-	uint8_t dta = 0;
-	if (HAL_GPIO_ReadPin(RPI_PIN_IN_PORT, RPI_PIN_IN_FOUNDCAT) == GPIO_PIN_SET)
-		dta |= RPI_PINCODE_I_FOUNDCAT;
-	else dta &= ~(RPI_PINCODE_I_FOUNDCAT);
-	// add more code as needed
-	return dta;
+_Bool rpi_foundCat() {
+	if (HAL_GPIO_ReadPin(RPI_PIN_IN_PORT, RPI_PIN_IN_FOUNDCAT) == GPIO_PIN_SET) return TRUE;
+	else return FALSE;
+
 }
 
 int rpi_serialDtaAvailable() { // returns zero if not available
@@ -61,7 +58,34 @@ int rpi_tcpipRespond(uint8_t isErr) { // send RESP pkt to client app. returns OK
 }
 */
 
-void rpi_RxCpltCallbackHandler() {
+void rpi_sendPin(int code) {
+	core_call_pendingOpCancel(opcode);
+	switch (code) {
+	case RPI_PINCODE_O_SCHEDULE_EXE:
+		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_RESET);
+		break;
+		/*
+	case RPI_PINCODE_O_SCHEDULE_END:
+		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_END, GPIO_PIN_SET);
+		break;
+		*/
+	case RPI_PINCODE_O_FIND_CAT_TIMEOUT:
+		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_RESET);
+		break;
+	}
+	core_call_pendingOpAdd(opcode, RPI_PIN_SEND_WAITING_TIME);
+}
+
+core_statRetTypeDef rpi_pendingOpTimeoutHandler() {
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_SET);
+	//HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_END, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_SET);
+	return OK;
+}
+
+static core_statRetTypeDef rpi_RxCpltCallbackHandler(UART_HandleTypeDef *huart) {
+	//if (huart->Instance != pUartHandle->Instance) return ERR;
+	//if (huart->Instance != USART2) return ERR;
 	UARTdta.available = 1; // mark available
 	UARTdta.type = rxBuf[0]; // copy data from buffer to internal var
 	for (int i = 0; i < DTA_LEN - 1; i++)
@@ -70,32 +94,11 @@ void rpi_RxCpltCallbackHandler() {
 	for (int i = 0; i < DTA_LEN + 1; i++) // clr buf
 		rxBuf[i] = 0;
 	HAL_UART_Receive_IT(pUartHandle, rxBuf, DTA_LEN); // restart rx
-}
-
-void rpi_sendPin(int code) {
-	core_call_pendingOpCancel(opcode);
-	switch (code) {
-	case RPI_PINCODE_O_SCHEDULE_EXE:
-		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_SET);
-		break;
-	case RPI_PINCODE_O_SCHEDULE_END:
-		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_END, GPIO_PIN_SET);
-		break;
-	case RPI_PINCODE_O_FIND_CAT_TIMEOUT:
-		HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_SET);
-		break;
-	}
-	core_call_pendingOpAdd(opcode, RPI_PIN_SEND_WAITING_TIME);
-}
-
-core_statRetTypeDef rpi_pendingOpTimeoutHandler() {
-	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_END, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_RESET);
 	return OK;
 }
 
 void rpi_init() {
+	// register pending op handler
 	core_statRetTypeDef retval = core_call_pendingOpRegister(&opcode, &rpi_pendingOpTimeoutHandler);
 	if (retval != OK) {
 #ifdef _TEST_MODE_ENABLED
@@ -105,6 +108,28 @@ void rpi_init() {
 		}
 #endif
 	}
+	// register UART intr handler
+	retval = core_call_uartHandlerRegister(&rpi_RxCpltCallbackHandler);
+	if (retval != OK) {
+#ifdef _TEST_MODE_ENABLED
+		core_dbgTx("\r\n?FAILED TO REGISTER RPI UART INTR HANDLER FUNCTION OF RPICOMM\r\n");
+		while (1) {
+
+		}
+#endif
+	}
+
+	// send pin data once, start and then timeout, set all the pins to HIGH(rpi conf: pull up to init
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_SET);
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_RESET);
+	HAL_Delay(1000);
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_SCHEDULE_EXE, GPIO_PIN_SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_RESET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(RPI_PIN_OUT_PORT, RPI_PIN_OUT_FIND_CAT_TIMEOUT, GPIO_PIN_SET);
+
+
 	UARTdta.available = 0;
 	for (int i = 0; i < 9; i++)
 		rxBuf[i] = 0;
