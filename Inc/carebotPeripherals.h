f75e1611d14dac715418a775a94fe947be546483
DS1TPT <49714844+DS1TPT@users.noreply.github.com>
2023-07-25 21:46:35 +0900

    내부 통신 타입 정의 추가

--- Inc/carebotPeripherals.h
+++ Inc/carebotPeripherals.h
@@ -1,7 +1,8 @@
 /**
   *********************************************************************************************
-  * NAME OF THE FILE : carebotPeripherals.h
-  * BRIEF INFORMATION: peripheral device driver
+  * NAME OF THE FILE : rpicomm.h
+  * BRIEF INFORMATION: for communications between stm32f411re nucleo board
+  * 			  	   and raspberry pi via GPIO pins and UART.
   *
   * Copyright (c) 2023 Lee Geon-goo.
   * All rights reserved.
@@ -11,8 +12,12 @@
   *********************************************************************************************
   */
 
-#ifndef CAREBOTPERIPHERALS_H
-#define CAREBOTPERIPHERALS_H
+/* check source file for header and container info. */
+
+#ifndef RPICOMM_H
+#define RPICOMM_H
+
+#include "main.h"
 
 #ifndef FALSE
 #define FALSE 0
@@ -22,35 +27,56 @@
 #endif
 
 /* definitions */
-// FIXED VAL
-#define IR_SNSR_FAR 0
-#define IR_SNSR_NEAR 1
-#define IR_SNSR_ERR -1
-#define IR_SNSR_MODE_OP 1
-#define IR_SNSR_MODE_FIND 2
-#define IR_SNSR_MODE_SNACK 3
-// config
-#define LASER_PORT GPIOA
-#define LASER_PIN GPIO_PIN_5
-#define VIB_SNSR_PORT GPIOB
-#define VIB_SNSR_PIN GPIO_PIN_0
-//#define LED_PORT GPIO
-//#define LED_PIN GPIO_PIN_
-#define IR_SNSR_POLL_TIMEOUT 10
-#define IR_SNSR_TRIG_DIST_OP 20.0
-#define IR_SNSR_TRIG_DIST_FIND 60.0
-#define IR_SNSR_TRIG_DIST_SNACK 15.0
+
+// pinIO code and config
+#define RPI_PIN_SEND_WAITING_TIME 1000
+#define DTA_LEN 8
+
+#define RPI_PINCODE_I_FOUNDCAT 0x01
+#define RPI_PINCODE_O_SCHEDULE_EXE 0x01
+#define RPI_PINCODE_O_SCHEDULE_END 0x02
+#define RPI_PINCODE_O_FIND_CAT_TIMEOUT 0x04
+
+// Data type header definitions
+#define TYPE_SCHEDULE_TIME 'T'
+#define TYPE_SCHEDULE_PATTERN 'P'
+#define TYPE_SCHEDULE_SNACK_INTERVAL 'N'
+#define TYPE_SCHEDULE_SPEED 'V'
+#define TYPE_SYS '!'
+#define TYPE_MANUAL_CTRL 'M'
+#define TYPE_SCHEDULE_DURATION 'D'
+#define TYPE_SCHEDULE_START '<'
+#define TYPE_SCHEDULE_END '>'
+#define TYPE_INTERNAL 'I'
+//#define TYPE_RESP 0xFF
+
+// pin code
+#define RPI_PIN_IN_PORT GPIOA
+#define RPI_PIN_IN_FOUNDCAT GPIO_PIN_10
+#define RPI_PIN_OUT_PORT GPIOA
+#define RPI_PIN_OUT_SCHEDULE_EXE GPIO_PIN_11
+//#define RPI_PIN_OUT_SCHEDULE_END GPIO_PIN_7
+#define RPI_PIN_OUT_FIND_CAT_TIMEOUT GPIO_PIN_12
 
 /* exported struct */
+struct SerialDta {
+	uint8_t available;
+	uint8_t type;
+	uint8_t container[8];
+};
 
 /* exported vars */
 
+
 /* exported func prototypes */
-void periph_setHandle(ADC_HandleTypeDef* ph);
-void periph_init();
-void periph_laser_on();
-void periph_laser_off();
-_Bool periph_isVibration();
-int periph_irSnsrChk(int mode);
+void rpi_setHandle(UART_HandleTypeDef* ph);
+void rpi_init();
+int rpi_getSerialDta(struct SerialDta* pDest); // returns zero if no data is available, even though flag will be set by callback handler...
+_Bool rpi_foundCat();
+void rpi_sendPin(int code);
+int rpi_serialDtaAvailable(); // returns zero if not available
+//int rpi_tcpipRespond(uint8_t isErr); // send RESP pkt to client app. returns 0 on success
+
+void rpi_msTimeoutHandler();
 
 #endif
